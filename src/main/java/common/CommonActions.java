2653bc07499eed808c6a716df4fc1a586ceb61c9
NickMAXHO <nick.lakhno@gmail.com>
2021-07-09 20:02:29 +0600

    Initial commit

---
+++ src/main/java/common/CommonActions.java
@@ -0,0 +1,40 @@
+package common;
+
+import com.sun.org.apache.bcel.internal.generic.SWITCH;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static common.Config.BROWSER_AND_PLATFORM;
+import static constants.Constant.TimeoutVariables.IMPLICIT_WAIT;
+
+public class CommonActions {
+    public static WebDriver createDriver(){
+        WebDriver driver = null;
+        switch (BROWSER_AND_PLATFORM) {
+            case "chrome_mac":
+                System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver");
+                driver = new ChromeDriver();
+                break;
+
+            case "chrome_windows":
+                System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");                driver = new ChromeDriver();
+                driver = new ChromeDriver();
+                break;
+
+            case "mozilla_windows"
+                System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
+                driver = new FirefoxDriver();
+                break;
+
+            default:
+                Assertions.fail("INCORRECT BROWSER NAME " + BROWSER_AND_PLATFORM);
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(IMPLICIT_WAIT, TimeUnit.SECONDS);
+        return driver;
+    }
+}
---
+++ src/main/java/common/Config.java
@@ -0,0 +1,13 @@
+package common;
+
+public class Config {
+
+    /**
+     * Specify browser-platform to test:
+     * CHROME_MAC
+     * CHROME_WINDOWS
+     * MOZILLA_MAC
+     * */
+    public static final String BROWSER_AND_PLATFORM = "CHROME-MAC";
+
+}
